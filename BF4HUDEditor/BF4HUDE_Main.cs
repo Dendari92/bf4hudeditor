13e87b18634b2048b026eb0d9111735a93fa968b
Dendari <dendari@hotmail.it>
2014-12-30 09:28:23 +0100

    First Commit

---
+++ BF4HUDEditor/BF4HUDE_Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BF4HUDEditor
+{
+    public partial class BF4HUDE_Main : Form
+    {
+        public BF4HUDE_Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
